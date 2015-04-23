1438c0823174d903de97af966ac1f0660e35c2f8
Rob Lewis <rob.y.lewis@gmail.com>
2015-04-23 13:32:39 -0400

    fix(library/algebra/sequence): remove sorrys

--- library/algebra/real.lean
+++ library/algebra/real.lean
@@ -1,5 +1,5 @@
 import algebra.sequence
-open sequence
+open sequence algebra
 
 --definition real := quot cauchy.to_setoid
 notation `ℝ` := quot cauchy.to_setoid --real
@@ -27,6 +27,75 @@ section real
                          quot.sound (mul.well_defined a b c d Hab Hcd)))
   infix `*` := mul
 
+  definition inv (x : ℝ) : ℝ := 
+    quot.lift_on x (λ a, quot.mk (a⁻¹)) (take a b : cauchy_sequence, take Hab : a ≡ b,
+                                           quot.sound (inv.well_defined a b Hab)) 
+  postfix `⁻¹` := inv
+
   -- prove ℝ is an ordered, dedekind-complete field
+
+  definition zero := quot.mk (cauchy_sequence.mk zero_seq zero_is_cauchy)
+  definition one := quot.mk (cauchy_sequence.mk one_seq one_is_cauchy)
+
+  definition le (x y : ℝ) : Prop := sorry
+  definition lt (x y : ℝ) : Prop := sorry
+
+  notation 0 := zero
+  notation 1 := one
+  infix `≤` := le
+  infix `<` := lt
+
+check @linear_ordered_field.mk
+
+  theorem add_assoc (x y z : ℝ) : x + y + z = x + (y + z) := sorry
+
+  theorem zero_add (x : ℝ) : 0 + x = x := sorry
+
+  theorem add_zero (x : ℝ) : x + 0 = x := sorry
+
+  theorem neg_cancel (x : ℝ) : -x + x = 0 := sorry
+
+  theorem add_comm (x y : ℝ) : x + y = y + x := sorry
+
+  theorem mul_assoc (x y z : ℝ) : x * y * z = x * (y * z) := sorry
+
+  theorem mul_comm (x y : ℝ) : x * y = y * x := sorry
+
+  theorem one_mul (x : ℝ) : 1 * x = x := sorry
+
+  theorem mul_one (x : ℝ) : x * 1 = x := sorry
+
+  theorem mul_add_eq_add_mul_left (x y z : ℝ) : x * (y + z) = x * y + x * z := sorry
+
+  theorem mul_add_eq_add_mul_right (x y z : ℝ) : (x + y) * z = x * z + y * z := sorry
+
+  theorem le_refl (x : ℝ) : x ≤ x := sorry
+
+  theorem le_trans (x y z : ℝ) (H1 : x ≤ y) (H2 : y ≤ z) : x ≤ z := sorry
+
+  theorem eq_of_le (x y : ℝ) (H1 : x ≤ y) (H2 : y ≤ x) : x = y := sorry
+
+  theorem lt_iff_le_and_ne (x y : ℝ) : x < y ↔ (x ≤ y ∧ x ≠ y) := sorry
+
+  theorem le_add (x y : ℝ) (H : x ≤ y) : ∀ z : ℝ, z + x ≤ z + y := sorry
+
+  theorem zero_ne_one' : (0 : ℝ) ≠ (1 : ℝ) := sorry
+
+  theorem mul_nonpos_of_nonpos (x y : ℝ) (Hx : x ≤ 0) (Hy : y ≤ 0) : x * y ≤ 0 := sorry
+
+  theorem mul_neg_of_neg (x y : ℝ) (Hx : x < 0) (Hy : y < 0) : x * y < 0 := sorry
+
+  theorem lt_or_eq_iff_le (x y : ℝ) : x ≤ y ↔ x < y ∨ x = y := sorry
+
+  theorem le_or_le (x y : ℝ) : x ≤ y ∨ y ≤ x := sorry
+
+  theorem inv_cancel_right (x : ℝ) : x ≠ (0 : ℝ) → x * x⁻¹ = 1 := sorry
+
+  theorem inv_cancel_left (x : ℝ) : x ≠ (0 : ℝ) → x⁻¹ * x = 1 := sorry
+  
+  definition real.to_lin_ord_field [instance] := linear_ordered_field.mk
+    add add_assoc 0 zero_add add_zero neg neg_cancel add_comm mul mul_assoc 1 one_mul mul_one mul_add_eq_add_mul_left
+    mul_add_eq_add_mul_right le le_refl le_trans eq_of_le lt lt_iff_le_and_ne le_add zero_ne_one' mul_nonpos_of_nonpos
+    mul_neg_of_neg lt_or_eq_iff_le le_or_le inv inv_cancel_right inv_cancel_left mul_comm
 
 end real
--- library/algebra/sequence.lean
+++ library/algebra/sequence.lean
@@ -25,7 +25,7 @@ definition rat.is_lin_ord [instance] : discrete_linear_ordered_field ℚ := rat_
 -- These belong elsewhere
 
 --why doesn't this exist?
-theorem le_of_eq {a b : ℚ} (H1 : a = b) : a ≤ b := 
+theorem le_of_eq {a b : ℚ} (H1 : a = b) : a ≤ b :=
   algebra.le_of_not_lt (take H : b < a, absurd !rfl (H1 ▸ (algebra.ne_of_lt H)))
 
 --------------------
@@ -45,7 +45,7 @@ theorem add_halves_helper (a : ℚ) : a / 2 + a / 2 = a :=
     ... = a / (q1 + q1) + a / (q1 + q1) : (zero_add q1) ▸ !refl
     ... = a : add_halves
 
-theorem rnip_helper (n : ℕ) (Hn : (nat.to_rat n) ≥ 0) : (nat.to_rat (succ n)) ≥ 0 := 
+theorem rnip_helper (n : ℕ) (Hn : (nat.to_rat n) ≥ 0) : (nat.to_rat (succ n)) ≥ 0 :=
   have H : nat.to_rat n + 1 = (nat.to_rat (succ n)), from !refl,
   H⁻¹ ▸ (algebra.add_nonneg Hn (algebra.le_of_lt algebra.zero_lt_one))
 
@@ -58,7 +58,7 @@ theorem rat_of_nat_is_pos (n : ℕ) (Hn : n > 0) : nat.to_rat n > 0 :=
       nat.to_rat n = nat.to_rat (succ l) : Hl
                ... > 0                   : Hl'')
 
-theorem rat_div_nat_pos {a : ℚ} {n : ℕ} (H : a > 0) (Hn : n > 0) : a / n > 0 := 
+theorem rat_div_nat_pos {a : ℚ} {n : ℕ} (H : a > 0) (Hn : n > 0) : a / n > 0 :=
   pos_div_of_pos_of_pos H (rat_of_nat_is_pos n Hn)
 
 
@@ -84,13 +84,17 @@ definition sub [reducible] (s t : rat_sequence) : rat_sequence :=
   s + (- t)
 local infix `-` := sub
 
-theorem seq_sub_self (s : rat_sequence) : s - s = λ n, 0 := 
+theorem seq_sub_self (s : rat_sequence) : s - s = λ n, 0 :=
   funext (take x,
     show s x - s x = 0, from !algebra.sub_self)
 
 definition mul [reducible] (s t : rat_sequence) : rat_sequence :=
   λ n : ℕ, (s n) * (t n)
 local infix `*` := mul
+
+definition inv [reducible] (s : rat_sequence) : rat_sequence :=
+  λ n : ℕ, 1 / (s n)
+local postfix `⁻¹` := inv
 
 definition vanishes (s : rat_sequence) : Prop :=
   ∀ ε : ℚ, ε > 0 → ∃ N : ℕ, ∀ n : ℕ, n ≥ N → abs (s n) < ε
@@ -138,7 +142,7 @@ theorem sum_vanishes_of_vanishes (s t : rat_sequence) (Hs : vanishes s) (Ht : va
         exact (Hb n Hbn)
   end
 
-theorem sum_converges_of_converges (s t : rat_sequence) (a b : ℚ) (Hs : converges_to s a) 
+theorem sum_converges_of_converges (s t : rat_sequence) (a b : ℚ) (Hs : converges_to s a)
     (Ht : converges_to t b) : converges_to (s + t) (a + b) :=
   begin
     rewrite ↑converges_to at *,
@@ -153,24 +157,22 @@ theorem sum_converges_of_converges (s t : rat_sequence) (a b : ℚ) (Hs : conver
         intro n, intro Hn,
         have H' : abs (add (λ k, s k - a) (λ k, t k - b) n) < ε, from Ha1 n Hn,
         have H'' : abs ((s n - a) + (t n - b)) < ε, from H',
-        rewrite [algebra.add.assoc at H'', {-a+_}algebra.add.comm at H'', 
+        rewrite [algebra.add.assoc at H'', {-a+_}algebra.add.comm at H'',
           algebra.add.assoc at H'', -(algebra.add.assoc (s n)) at H'', -neg_add_rev at H''],
         exact H''
     end
-
-
 ----
 -- Prove all cauchy seqs are bounded
 ----
-           
+
 -- create list of nats from 0 to N - 1
 definition nat_list : ℕ → list ℕ
   | 0 := [0]
   | (succ n) := cons n (nat_list n)
 
-theorem in_nat_list : ∀{N : ℕ}, ∀ {n}, n < N → n ∈ nat_list N 
+theorem in_nat_list : ∀{N : ℕ}, ∀ {n}, n < N → n ∈ nat_list N
   | 0 n Hn := absurd Hn !not_lt_zero
-  | (succ a) n Hn := begin 
+  | (succ a) n Hn := begin
       apply (or.elim (nat.lt_or_eq_of_le (le_of_lt_succ Hn))),
         intro Ho,
         apply mem_cons_of_mem,
@@ -182,17 +184,17 @@ theorem in_nat_list : ∀{N : ℕ}, ∀ {n}, n < N → n ∈ nat_list N
 
 definition rat_list_max : list ℚ → ℚ  -- default if l is empty, else max l
   | []        := 0
-  | [h]       := abs h  
+  | [h]       := abs h
   | (h :: t)  := algebra.max (abs h) (rat_list_max t)
 
 theorem rat_list_max_of_singleton (h : ℚ) : rat_list_max [h] = abs h := rfl
 
 theorem lt_rat_list_max_of_in_rat_list : ∀{l : list ℚ}, ∀{q : ℚ}, q ∈ l → abs q ≤ rat_list_max l
   | []               q Hq  :=  absurd Hq !not_mem_nil
-  | [h]              q Hq  := 
-    have H1 : q = h, from mem_singleton Hq, 
-     le_of_eq (H1⁻¹ ▸ !rat_list_max_of_singleton) 
-  | (h :: (h' :: t)) q Hq  :=  
+  | [h]              q Hq  :=
+    have H1 : q = h, from mem_singleton Hq,
+     le_of_eq (H1⁻¹ ▸ !rat_list_max_of_singleton)
+  | (h :: (h' :: t)) q Hq  :=
     or.elim (iff.mp !mem_cons_iff Hq)
       (assume H1 : q = h,
         calc
@@ -240,8 +242,8 @@ theorem bounded_of_cauchy {s : rat_sequence} (H : cauchy s) : bounded s :=
           apply (le_of_lt (calc
             abs (s n) - abs (s N) ≤ abs (s n - s N) : abs_sub_abs_le_abs_sub
             ... < 1 : Ha))
-  end 
- 
+  end
+
 theorem sub_dist (a b c d : ℚ) : (a + b) - (c + d) = a + b - c - d :=
   !sub_add_eq_sub_sub
 
@@ -263,7 +265,7 @@ theorem cauchy_of_converges (s : rat_sequence) (H : converges s) : cauchy s :=
           {intro m, intro n, intro Hm, intro Hn,
           have HNm : abs (s m - a) < ε / 2, from HN m Hm,
           have HNn : abs (a - s n) < ε / 2, from !abs_sub ▸ (HN n Hn),
-          rewrite [ -(add_sub_add_left_eq_sub _ _ a), sub_dist, {a+_}algebra.add.comm, 2 algebra.add.assoc, 
+          rewrite [ -(add_sub_add_left_eq_sub _ _ a), sub_dist, {a+_}algebra.add.comm, 2 algebra.add.assoc,
             -(algebra.add.assoc a), {a+_}algebra.add.comm, algebra.add.assoc, -algebra.add.assoc (s m)],
           rewrite -(add_halves_helper ε),
           apply algebra.lt_of_le_of_lt,
@@ -273,9 +275,16 @@ theorem cauchy_of_converges (s : rat_sequence) (H : converges s) : cauchy s :=
           exact HNn}
   end
 
+-- classical-ish?
+theorem nonzero_of_not_vanishes (s : rat_sequence) (H : cauchy s) (Hv : ¬vanishes s) :
+        ∃ B N : ℕ, ∀ n : ℕ, n > N → abs (s n) < B := sorry
+/-  begin
+    rewrite [↑cauchy at *, ↑vanishes at *],
+  end-/
+
 definition seq.equiv (s t : rat_sequence) : Prop :=
-  vanishes (s - t)-- ∀ ε : ℚ, ε > 0 → ∃ N : ℕ, ∀ n : ℕ, n ≥ N → abs (s n - t n) < ε
---notation p `≡` q := seq.equiv p q
+  vanishes (s - t)
+
 
 -- show seq.equiv is an equivalence relation
 
@@ -323,7 +332,7 @@ theorem seq.equiv.trans {r s t : rat_sequence} (H1 : seq.equiv r s) (H2 : seq.eq
           intros [n, Hn],
           rewrite -(add_halves_helper ε),
           apply algebra.lt_of_le_of_lt,
-          rewrite [↑sub at *, ↑add at *, -(algebra.add_zero (r n)), -(algebra.sub_self (s n)), add.comm (s n), 
+          rewrite [↑sub at *, ↑add at *, -(algebra.add_zero (r n)), -(algebra.sub_self (s n)), add.comm (s n),
             -algebra.add.assoc, algebra.add.assoc],
           apply abs_add_le_abs_add_abs,
           apply algebra.add_lt_add,
@@ -333,16 +342,17 @@ theorem seq.equiv.trans {r s t : rat_sequence} (H1 : seq.equiv r s) (H2 : seq.eq
           exact (Hb n Hbn)
   end
 end rat
+
+
+
 section cauchy
 
 record cauchy_sequence : Type :=
   (seq : rat_sequence) (is_cauchy : cauchy seq)
-  
---definition cauchy_sequence := {s : rat_sequence | cauchy s}
+
 
 definition cauchy.to_rat [reducible] (s : cauchy_sequence) : ℕ → ℚ :=
   cauchy_sequence.seq s
-           --subtype.rec_on s (λ t H, t)
 
 local attribute cauchy.to_rat [coercion]
 
@@ -365,7 +375,7 @@ theorem add_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s + t) :=
         fapply exists.intro,
         exact (max a b),
         intros [m, n, Hm, Hn],
-        rewrite [↑add, {s n + _}algebra.add.comm, sub_add_eq_sub_sub, (algebra.add.assoc (s m)), 
+        rewrite [↑add, {s n + _}algebra.add.comm, sub_add_eq_sub_sub, (algebra.add.assoc (s m)),
         algebra.add.comm, -algebra.add.assoc],
         apply algebra.lt_of_le_of_lt,
         apply abs_add_le_abs_add_abs,
@@ -401,7 +411,72 @@ theorem pos_div_helper {ε N : ℚ} (Hε : ε > 0) (HN : N > 0) : ε / ((2 : ℚ
 theorem sub_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s - t) :=
   add_cauchy_of_cauchy (s) (cauchy_sequence.mk (-t) (neg_cauchy_of_cauchy t))
 
--- 4.3 sec
+-- uses nonzero_of_not_vanishes
+theorem inv_cauchy_of_nonzero_cauchy (s : cauchy_sequence) (Hs : ¬vanishes s) : cauchy s⁻¹ :=
+  sorry
+  /-begin
+    have Cs : cauchy s, from cauchy_sequence.is_cauchy s,
+    rewrite [↑cauchy at *, ↑vanishes at *],
+  end-/
+
+-- helper lemmas for following
+lemma mul_cauchy_helper_1 (s t : cauchy_sequence) (ε : ℚ) (Ns : ℚ) (HNs : ∀ n, abs (s n) ≤ Ns)
+                           (Hε : ε > 0) (Nseq0 : Ns = (0: ℚ))
+      : (∃ (N : ℕ), ∀ (m n : ℕ), m ≥ N → n ≥ N → abs (s m * t m - s n * t n) < ε) :=
+  begin
+    fapply exists.intro, exact 0, intros [m, n, Hm, Hn],
+    have H0 : ∀ k, abs (s k) = 0, from (take k, abs_eq_zero_of_abs_le_zero (Nseq0 ▸ (HNs k))),
+    apply (calc
+      abs (s m * t m - s n * t n) ≤ abs (s m * t m) + abs (-(s n * t n)) : abs_add_le_abs_add_abs
+      ... = abs (s m) * abs (t m) + abs (s n) * abs (t n) : by rewrite [abs_neg, 2 abs_mul]
+      ... = 0 : by rewrite [2 H0, 2 algebra.zero_mul, algebra.zero_add]
+      ... < ε : Hε)
+  end
+
+lemma mul_cauchy_helper_2 (s t : cauchy_sequence) (ε : ℚ) (Nt : ℚ) (HNt : ∀ n, abs (t n) ≤ Nt)
+                           (Hε : ε > 0) (Nteq0 : Nt = (0: ℚ))
+      : (∃ (N : ℕ), ∀ (m n : ℕ), m ≥ N → n ≥ N → abs (s m * t m - s n * t n) < ε) :=
+  begin
+    fapply exists.intro, exact 0, intros [m, n, Hm, Hn],
+    have H0 : ∀ k, abs (t k) = 0, from (take k, abs_eq_zero_of_abs_le_zero (Nteq0 ▸ (HNt k))),
+    apply (calc
+      abs (s m * t m - s n * t n) ≤ abs (s m * t m) + abs (-(s n * t n)) : abs_add_le_abs_add_abs
+      ... = abs (s m) * abs (t m) + abs (s n) * abs (t n) : by rewrite [abs_neg, 2 abs_mul]
+      ... = 0 : by rewrite [2 H0, 2 algebra.mul_zero, algebra.zero_add]
+      ... < ε : Hε)
+  end
+
+lemma mul_cauchy_helper_3 (s t : cauchy_sequence) (Hs : cauchy s) (Ht : cauchy t) (ε : ℚ) (Ns Nt : ℚ)
+                           (HNs : ∀ n, abs (s n) ≤ Ns) (HNt : ∀ n, abs (t n) ≤ Nt)
+                           (Hε : ε > 0) (Nsge0 : Ns > (0 : ℚ)) (Ntge0 : Nt > (0: ℚ))
+      : (∃ (N : ℕ), ∀ (m n : ℕ), m ≥ N → n ≥ N → abs (s m * t m - s n * t n) < ε) :=
+  begin
+    apply exists.elim, exact (Hs (ε / ((2 : ℚ) * Nt)) (pos_div_helper Hε Ntge0)), intros [Sε, HSε],
+    apply exists.elim, exact (Ht (ε / ((2 : ℚ) * Ns)) (pos_div_helper Hε Nsge0)), intros [Tε, HTε],
+    fapply exists.intro,
+    exact (max Sε Tε),
+    intros [m, n, Hm, Hn],
+    rewrite [-algebra.add_zero (s m * t m), -algebra.sub_self (s m * t n), algebra.add.comm (s m * t n),
+      -algebra.add.assoc, algebra.add.assoc, -algebra.mul_sub_left_distrib,
+      -algebra.mul_sub_right_distrib],
+    have HTε' : abs (t m - t n) < ε / (2 * Ns), from HTε _ _ (le.trans !le_max_right Hm) (le.trans !le_max_right Hn),
+    have HSε' : abs (s m - s n) < ε / (2 * Nt), from HSε _ _ (le.trans !le_max_left Hm) (le.trans !le_max_left Hn),
+    have Habs1 : abs (s m) * abs (t m - t n) < ε / 2, from calc
+      abs (s m) * abs (t m - t n) ≤ Ns * abs (t m - t n) : algebra.mul_le_mul_of_nonneg_right !HNs !abs_nonneg
+      ... < Ns * (ε / ((2 : ℚ) * Ns)) : algebra.mul_lt_mul_of_pos_left HTε' Nsge0
+      ... = ε / 2 : sorry, -- simp
+    have Habs2 : abs (s m - s n) * abs (t n) < ε / 2, from calc
+      abs (s m - s n) * abs (t n) ≤  abs (s m - s n) * Nt : algebra.mul_le_mul_of_nonneg_left !HNt !abs_nonneg
+      ... < (ε / ((2 : ℚ) * Nt)) * Nt : algebra.mul_lt_mul_of_pos_right HSε' Ntge0
+      ... = ε / 2 : sorry, -- simp
+    apply (calc
+      abs (s m * (t m - t n) + (s m - s n) * t n) ≤ abs (s m * (t m - t n)) + abs ((s m - s n) * t n) : abs_add_le_abs_add_abs
+      ... = abs (s m) * abs (t m - t n) + abs (s m - s n) * abs (t n) : by rewrite 2 abs_mul
+      ... < ε / 2 + ε / 2 : add_lt_add_of_lt_of_lt Habs1 Habs2
+      ... = ε : add_halves_helper)
+  end
+
+-- splitting into lemmas makes it around 4 sec
 theorem mul_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s * t) :=
   begin
     have Hs : cauchy s, from cauchy_sequence.is_cauchy s,
@@ -415,10 +490,32 @@ theorem mul_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s * t) :=
     have Nt0 : (0 : ℚ) ≤ Nt, from algebra.le.trans (algebra.abs_nonneg (t 0)) (HNt 0),
     have Nsor : Ns = (0 : ℚ) ∨ 0 < Ns, from algebra.eq_or_lt_of_not_lt (algebra.not_lt_of_le Ns0),
     have Ntor : Nt = (0 : ℚ) ∨ 0 < Nt, from algebra.eq_or_lt_of_not_lt (algebra.not_lt_of_le Nt0),
-    apply (or.elim Nsor), 
+    apply (or.elim Nsor),
+    {intro Nseq0, exact (mul_cauchy_helper_1 s t ε Ns HNs Hε Nseq0)},
+    intro Nsge0,
+    apply (or.elim Ntor),
+    {intro Nteq0, exact (mul_cauchy_helper_2 s t ε Nt HNt Hε Nteq0)},
+    {intro Ntge0, exact (mul_cauchy_helper_3 s t Hs Ht ε Ns Nt HNs HNt Hε Nsge0 Ntge0)}
+  end
+
+-- 4.3 sec
+/-theorem mul_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s * t) :=
+  begin
+    have Hs : cauchy s, from cauchy_sequence.is_cauchy s,
+    have Ht : cauchy t, from cauchy_sequence.is_cauchy t,
+    rewrite [↑cauchy at *, ↑mul],
+    apply exists.elim, exact (bounded_of_cauchy Hs),
+    intros [Ns, HNs],
+    apply exists.elim, exact (bounded_of_cauchy Ht),
+    intros [Nt, HNt, ε, Hε],
+    have Ns0 : (0 : ℚ) ≤ Ns, from algebra.le.trans (algebra.abs_nonneg (s 0)) (HNs 0),
+    have Nt0 : (0 : ℚ) ≤ Nt, from algebra.le.trans (algebra.abs_nonneg (t 0)) (HNt 0),
+    have Nsor : Ns = (0 : ℚ) ∨ 0 < Ns, from algebra.eq_or_lt_of_not_lt (algebra.not_lt_of_le Ns0),
+    have Ntor : Nt = (0 : ℚ) ∨ 0 < Nt, from algebra.eq_or_lt_of_not_lt (algebra.not_lt_of_le Nt0),
+    apply (or.elim Nsor),
     {intro Nseq0,
     fapply exists.intro, exact 0, intros [m, n, Hm, Hn],
-    have H0 : ∀ k, abs (s k) = 0, from (take k, abs_eq_zero_of_abs_le_zero (Nseq0 ▸ (HNs k))), 
+    have H0 : ∀ k, abs (s k) = 0, from (take k, abs_eq_zero_of_abs_le_zero (Nseq0 ▸ (HNs k))),
     apply (calc
       abs (s m * t m - s n * t n) ≤ abs (s m * t m) + abs (-(s n * t n)) : abs_add_le_abs_add_abs
       ... = abs (s m) * abs (t m) + abs (s n) * abs (t n) : by rewrite [abs_neg, 2 abs_mul]
@@ -426,9 +523,9 @@ theorem mul_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s * t) :=
       ... < ε : Hε)},
     intro Nsge0,
     apply (or.elim Ntor),
-    {intro Nteq0,   
+    {intro Nteq0,
     fapply exists.intro, exact 0, intros [m, n, Hm, Hn],
-    have H0 : ∀ k, abs (t k) = 0, from (take k, abs_eq_zero_of_abs_le_zero (Nteq0 ▸ (HNt k))), 
+    have H0 : ∀ k, abs (t k) = 0, from (take k, abs_eq_zero_of_abs_le_zero (Nteq0 ▸ (HNt k))),
     apply (calc
       abs (s m * t m - s n * t n) ≤ abs (s m * t m) + abs (-(s n * t n)) : abs_add_le_abs_add_abs
       ... = abs (s m) * abs (t m) + abs (s n) * abs (t n) : by rewrite [abs_neg, 2 abs_mul]
@@ -441,27 +538,55 @@ theorem mul_cauchy_of_cauchy (s t : cauchy_sequence) : cauchy (s * t) :=
     exact (max Sε Tε),
     intros [m, n, Hm, Hn],
     rewrite [-algebra.add_zero (s m * t m), -algebra.sub_self (s m * t n), algebra.add.comm (s m * t n),
-      -algebra.add.assoc, algebra.add.assoc, -algebra.mul_sub_left_distrib, 
+      -algebra.add.assoc, algebra.add.assoc, -algebra.mul_sub_left_distrib,
       -algebra.mul_sub_right_distrib],
     have HTε' : abs (t m - t n) < ε / (2 * Ns), from HTε _ _ (le.trans !le_max_right Hm) (le.trans !le_max_right Hn),
     have HSε' : abs (s m - s n) < ε / (2 * Nt), from HSε _ _ (le.trans !le_max_left Hm) (le.trans !le_max_left Hn),
     have Habs1 : abs (s m) * abs (t m - t n) < ε / 2, from calc
       abs (s m) * abs (t m - t n) ≤ Ns * abs (t m - t n) : algebra.mul_le_mul_of_nonneg_right !HNs !abs_nonneg
-      ... < Ns * (ε / ((2 : ℚ) * Ns)) : algebra.mul_lt_mul_of_pos_left HTε' Nsge0 
-      ... = ε / 2 : sorry, -- simp    
+      ... < Ns * (ε / ((2 : ℚ) * Ns)) : algebra.mul_lt_mul_of_pos_left HTε' Nsge0
+      ... = ε / 2 : sorry, -- simp
     have Habs2 : abs (s m - s n) * abs (t n) < ε / 2, from calc
       abs (s m - s n) * abs (t n) ≤  abs (s m - s n) * Nt : algebra.mul_le_mul_of_nonneg_left !HNt !abs_nonneg
-      ... < (ε / ((2 : ℚ) * Nt)) * Nt : algebra.mul_lt_mul_of_pos_right HSε' Ntge0 
+      ... < (ε / ((2 : ℚ) * Nt)) * Nt : algebra.mul_lt_mul_of_pos_right HSε' Ntge0
       ... = ε / 2 : sorry, -- simp
     apply (calc
       abs (s m * (t m - t n) + (s m - s n) * t n) ≤ abs (s m * (t m - t n)) + abs ((s m - s n) * t n) : abs_add_le_abs_add_abs
       ... = abs (s m) * abs (t m - t n) + abs (s m - s n) * abs (t n) : by rewrite 2 abs_mul
       ... < ε / 2 + ε / 2 : add_lt_add_of_lt_of_lt Habs1 Habs2
       ... = ε : add_halves_helper)}
-  end
+  end-/
 
 
 -- show setoid cauchy_sequence
+
+definition zero_seq : rat_sequence := λ n, 0
+theorem zero_is_cauchy : cauchy zero_seq :=
+  begin
+    rewrite [↑zero_seq, ↑cauchy],
+    intros [ε, Hε],
+    fapply exists.intro,
+    exact 0,
+    intros [m, n, Hm, Hn],
+    rewrite [algebra.sub_zero, abs_zero],
+    exact Hε
+  end
+
+definition one_seq : rat_sequence := λ n, 1
+theorem one_is_cauchy : cauchy one_seq :=
+  begin
+    rewrite [↑one_seq, ↑cauchy],
+    intros [ε, Hε],
+    fapply exists.intro,
+    exact 0,
+    intros [m, n, Hm, Hn],
+    rewrite [algebra.sub_self, abs_zero],
+    exact Hε
+  end
+
+-- this isn't true, is it?
+theorem vanishes_is_decidable [instance] (s : rat_sequence) : decidable (vanishes s) :=
+  sorry
 
 definition cauchy.add (s t : cauchy_sequence) : cauchy_sequence :=
   cauchy_sequence.mk (s + t) (add_cauchy_of_cauchy _ _) -- why doesn't this work with ! ?
@@ -475,10 +600,16 @@ definition cauchy.sub (s t : cauchy_sequence) : cauchy_sequence :=
 definition cauchy.mul (s t : cauchy_sequence) : cauchy_sequence :=
   cauchy_sequence.mk (s * t) (mul_cauchy_of_cauchy _ _)
 
+definition cauchy.inv (s : cauchy_sequence) : cauchy_sequence :=
+  decidable.cases_on (vanishes_is_decidable s)
+    (take H, cauchy_sequence.mk zero_seq zero_is_cauchy)
+    (take H, cauchy_sequence.mk (s⁻¹) (inv_cauchy_of_nonzero_cauchy _ H))
+
 infix `+` := cauchy.add
 prefix `-` := cauchy.neg
 infix `-` := cauchy.sub
 infix `*` := cauchy.mul
+postfix `⁻¹` := cauchy.inv
 
 definition cauchy.equiv [reducible] (s t : cauchy_sequence) : Prop :=
   seq.equiv s t
@@ -495,7 +626,7 @@ theorem cauchy.equiv.symm (s t : cauchy_sequence) (H : s ≡ t) : t ≡ s :=
 theorem cauchy.equiv.trans (r s t : cauchy_sequence) (H1 : r ≡ s) (H2 : s ≡ t) : r ≡ t :=
   seq.equiv.trans H1 H2
 
-theorem cauchy.equiv.is_equiv : equivalence cauchy.equiv := 
+theorem cauchy.equiv.is_equiv : equivalence cauchy.equiv :=
   mk_equivalence cauchy.equiv cauchy.equiv.refl cauchy.equiv.symm cauchy.equiv.trans
 
 definition cauchy.to_setoid [instance] : setoid cauchy_sequence :=
@@ -503,22 +634,32 @@ definition cauchy.to_setoid [instance] : setoid cauchy_sequence :=
 
 end cauchy
 
-
 /-definition real := quot cauchy.to_setoid
 
 constants q r s : cauchy_sequence
 check q * s-/
 
-theorem add.well_defined (q r s t : cauchy_sequence) (H1 : q ≡ s) (H2 : r ≡ t) : 
-   q + r ≡ s + t := sorry
+theorem seq_dist_sub (q r s t : cauchy_sequence) : cauchy.sub (cauchy.add q r) (cauchy.add s t) = q + r - s - t := sorry
+
+theorem seq_comm (q r s t : cauchy_sequence) : q + r - s - t = (q - s) + (r - t) := sorry
+
+theorem blah (q r s t : cauchy_sequence) (H1 : vanishes (q - s)) (H2 : vanishes (r - t)) : vanishes (cauchy.sub (cauchy.add (q)  (r))  (cauchy.add (s)  (t))) := sorry
+
+theorem add.well_defined (q r s t : cauchy_sequence) (H1 : q ≡ s) (H2 : r ≡ t) :
+   q + r ≡ s + t :=
+  /- begin
+     rewrite [↑cauchy.equiv at *, ↑seq.equiv at *],
+     apply blah,
+   end-/ sorry
 
 theorem neg.well_defined (q r : cauchy_sequence) (H1 : q ≡ r) : -q ≡ -r := sorry
 
-theorem sub.well_defined (q r s t : cauchy_sequence) (H1 : q ≡ s) (H2 : r ≡ t) : 
+theorem sub.well_defined (q r s t : cauchy_sequence) (H1 : q ≡ s) (H2 : r ≡ t) :
   q - r ≡ s - t := sorry
 
 theorem mul.well_defined (q r s t : cauchy_sequence) (H1 : q ≡ s) (H2 : r ≡ t) :
    q * r ≡ s * t := sorry
 
+theorem inv.well_defined (q r : cauchy_sequence) (H1 : q ≡ r) : q⁻¹ ≡ r⁻¹ := sorry
+
 end sequence
- 
