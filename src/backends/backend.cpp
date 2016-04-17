1ac04a818bcaed272a65b3df59455fcf6d71c045
Jared Roesch <roeschinc@gmail.com>
2016-04-17 00:48:44 -0700

    feat(backends): push forward on simplification
    also improve code output

--- src/backends/backend.cpp
+++ src/backends/backend.cpp
@@ -1,13 +1,219 @@
-/*
+    /*
 Copyright (c) 2016 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Jared Roesch
 */
 #include <iostream>
+#include <memory>
+#include <utility>
+#include "backend.h"
+#include "kernel/environment.h"
+#include "kernel/inductive/inductive.h"
+#include "kernel/instantiate.h"
+#include "used_names.h"
+#include "util/fresh_name.h"
+#include "util/name.h"
+#include "util/name_set.h"
 
 namespace lean {
-  void backend_is_alive() {
-      std::cout << "I'm alive!" << std::endl;
-  }
+
+void print_decl(environment const & env, declaration const & d) {
+    if (d.is_axiom()) {
+        std::cout << "axiom: " << std::endl;
+        std::cout << d.get_name() << std::endl;
+    } else if (d.is_definition()) {
+        std::cout << "def: "
+        << d.get_name()
+        << " : " << d.get_type() << " := "
+        << d.get_value() << std::endl;
+    } else if (d.is_constant_assumption()) {
+        // bool is_definition() const;
+        // bool is_axiom() const;
+        // bool is_theorem() const;
+        // bool is_constant_assumption() const;
+
+        std::cout << "constant assumption: " << std::endl;
+        std::cout << d.get_name() << " : " << d.get_type() << std::endl;
+        bool is_inductive;
+
+        if (inductive::is_intro_rule(env, d.get_name())) {
+            is_inductive = true;
+        } else {
+            is_inductive = false;
+        }
+        std::cout << is_inductive << std::endl;
+
+    } else if (d.is_theorem()) {
+        std::cout << "theorem" << std::endl;
+    }
+}
+
+backend::backend(environment const & env, optional<std::string> main_fn)
+    : m_env(env) {
+    auto main_name = name(main_fn.value());
+    auto main = env.get(main_name);
+    // print_decl(env, main);
+    used_defs used_names(env);
+    used_names.names_in_decl(main);
+    std::cout << "-----------debug---------------" << main_fn.value() << std::endl;
+    auto decls_to_compile = std::vector<declaration>();
+    used_names.m_used_names.for_each([&] (name const &n) {
+        decls_to_compile.push_back(env.get(n));
+    });
+
+    for (auto decl : decls_to_compile) {
+        this->compile_decl(decl);
+        //print_decl(env, decl);
+    }
+}
+
+void backend::compile_decl(declaration const & d) {
+    if (d.is_axiom()) {
+        std::cout << "axiom: " << std::endl;
+        std::cout << d.get_name() << std::endl;
+    } else if (d.is_definition()) {
+        auto ty = d.get_type();
+        auto value = d.get_value();
+
+        std::vector<name> args;
+        auto se = this->compile_body(args, value);
+        auto p = proc(d.get_name(), args, se);
+        this->add_proc(p);
+    } else if (d.is_constant_assumption()) {
+        std::cout << "constant assumption: " << std::endl;
+        std::cout << d.get_name() << " : " << d.get_type() << std::endl;
+        bool is_inductive;
+
+        if (inductive::is_intro_rule(this->m_env, d.get_name())) {
+            is_inductive = true;
+        } else {
+            is_inductive = false;
+        }
+        std::cout << is_inductive << std::endl;
+    } else if (d.is_theorem()) {
+        std::cout << "theorem" << std::endl;
+    } else {
+        std::cout << "unhandled case" << d.get_name() << std::endl;
+
+    }
+}
+
+shared_ptr<simple_expr> backend::compile_body(std::vector<name> & args, expr const & e) {
+    if (is_lambda(e)) {
+        buffer<expr> ls;
+        auto ex = e;
+        while (is_binding(ex)) {
+            expr d = instantiate_rev(binding_domain(ex), ls.size(), ls.data());
+            auto n = mk_fresh_name();
+            expr l = mk_local(n, binding_name(ex), d, binding_info(ex));
+            args.push_back(n);
+            ls.push_back(l);
+            ex = binding_body(ex);
+        }
+        ex = instantiate_rev(ex, ls.size(), ls.data());
+        return this->compile_expr(ex);
+    } else {
+        return this->compile_expr(e);
+    }
+}
+
+shared_ptr<simple_expr> backend::compile_expr(expr const & e) {
+    std::vector<binding> bindings;
+    auto se = compile_expr(e, bindings);
+    for (auto binding : bindings) {
+        std::cout << binding.first << " : " << binding.second << std::endl;
+    }
+
+    return let_binding(bindings, se);
+}
+
+shared_ptr<simple_expr> backend::compile_expr(expr const & e, std::vector<binding> & bindings) {
+    std::cout << "exp: " << e << std::endl;
+    switch (e.kind()) {
+        case expr_kind::Local:
+            return shared_ptr<simple_expr>(new simple_expr_error("local"));
+        case expr_kind::Meta:
+            std::cout << "meta: not supported" << std::endl;
+            break;
+        case expr_kind::Var:
+            std::cout<< "var: not supported" << std::endl;
+            break;
+        case expr_kind::Sort:
+            std::cout<< "sort: not supported" << std::endl;
+            break;
+        case expr_kind::Constant:
+            return this->compile_expr_const(e);
+        case expr_kind::Macro:
+            return this->compile_expr_macro(e, bindings);
+        case expr_kind::Lambda:
+            return this->compile_expr_lambda(e, bindings);
+        case expr_kind::Pi:
+            std::cout<< "pi: not supported" << std::endl;
+            break;
+        case expr_kind::App:
+            return this->compile_expr_app(e, bindings);
+        case expr_kind::Let:
+            break;
+    }
+    throw new std::string("app: not supported");
+}
+
+shared_ptr<simple_expr> backend::compile_expr_const(expr const & e) {
+    name n = name(const_name(e));
+    return shared_ptr<simple_expr>(new simple_expr_var(n));
+}
+
+shared_ptr<simple_expr> backend::compile_expr_app(expr const & e, std::vector<binding> & bindings) {
+    buffer<expr> args;
+    expr const & f = get_app_args(e, args);
+    unsigned nargs = args.size();
+    std::vector<name> names;
+    for (unsigned i = 0; i < nargs; i++) {
+         std::cout << args[i] << std::endl;
+         auto n = mk_fresh_name();
+         this->bind_name(n, args[i], bindings);
+         names.push_back(n);
+    }
+
+    auto callee_name = name("callee_thing");
+    this->bind_name(callee_name, f, bindings);
+
+    return shared_ptr<simple_expr>(new simple_expr_call(callee_name, names));
+}
+
+shared_ptr<simple_expr> backend::compile_expr_macro(expr const & e, std::vector<binding> & bindings) {
+    auto def = macro_def(e);
+    std::cout << "macro: not supported(";
+    def.display(std::cout);
+    std::cout << ")" << std::endl;
+    return shared_ptr<simple_expr>(new simple_expr_error(std::string("macro_here")));
+}
+
+shared_ptr<simple_expr> backend::compile_expr_lambda(expr const & e, std::vector<binding> & bindings) {
+    return shared_ptr<simple_expr>(new simple_expr_error(std::string("macro_here")));
+}
+
+
+shared_ptr<simple_expr> backend::compile_error(std::string msg) {
+    return shared_ptr<simple_expr>(new simple_expr_error(msg));
+}
+
+void backend::bind_name(name n, expr const & e, std::vector<binding> & bindings) {
+    auto simple_arg = this->compile_expr(e, bindings);
+    bindings.push_back(binding(n, simple_arg));
+}
+
+void backend::add_proc(proc p) {
+    this->m_procs.push_back(p);
+}
+
+shared_ptr<simple_expr> let_binding(std::vector<binding> bindings, shared_ptr<simple_expr> se) {
+    if (bindings.empty()) {
+        return se;
+    } else {
+        return std::shared_ptr<simple_expr>(new simple_expr_let(bindings, se));
+    }
+}
+
 }
--- src/backends/simple_expr.h
+++ src/backends/simple_expr.h
@@ -7,33 +7,89 @@ Author: Jared Roesch
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include "kernel/environment.h"
 
+
 namespace lean  {
+    using std::shared_ptr;
+
+    enum simple_expr_kind {
+        SVar,
+        Let,
+        Call,
+        Error,
+    };
+
     struct simple_expr {
+        simple_expr_kind m_kind;
+
+        virtual simple_expr_kind kind() const = 0;
         friend std::ostream& operator<<(std::ostream& os, const simple_expr & e);
         void display(std::ostream& os) const;
     };
 
-    typedef pair<name, simple_expr> binding;
+    typedef pair<name, shared_ptr<simple_expr>> binding;
 
     struct simple_expr_var : simple_expr {
         name m_name;
         simple_expr_var(name m_name) : m_name(m_name) {}
+        virtual simple_expr_kind kind() const { return simple_expr_kind::SVar; }
     };
 
     struct simple_expr_let : simple_expr {
-        std::vector<std::pair<name, simple_expr>> m_name;
-        simple_expr m_body;
+        std::vector<binding> m_bindings;
+        shared_ptr<simple_expr> m_body;
+        simple_expr_let(std::vector<binding> bindings, shared_ptr<simple_expr> m_body)
+            : m_bindings(bindings), m_body(m_body) {}
+        virtual simple_expr_kind kind() const { return simple_expr_kind::Let; }
     };
 
     struct simple_expr_call : simple_expr {
         name m_name;
-        std::vector<simple_expr> m_args;
+        std::vector<name> m_args;
+        simple_expr_call(name m_name, std::vector<name> m_args)
+            : m_name(m_name), m_args(m_args) {
+            this->m_kind = simple_expr_kind::Call;
+        }
+        virtual simple_expr_kind kind() const { return simple_expr_kind::Call; }
     };
 
     struct simple_expr_error : simple_expr {
         std::string m_error_msg;
         simple_expr_error(std::string msg) : m_error_msg(msg) {}
+        virtual simple_expr_kind kind() const { return simple_expr_kind::Error; }
     };
+
+    inline bool simple_is_let(simple_expr const & e) {
+        return e.kind() == simple_expr_kind::Let;
+    }
+    // inline bool is_var(expr_ptr e)         { return e->kind() == expr_kind::Var; }
+    // inline bool is_constant(expr_ptr e)    { return e->kind() == expr_kind::Constant; }
+    // inline bool is_local(expr_ptr e)       { return e->kind() == expr_kind::Local; }
+    // inline bool is_metavar(expr_ptr e)     { return e->kind() == expr_kind::Meta; }
+    // inline bool is_macro(expr_ptr e)       { return e->kind() == expr_kind::Macro; }
+    // inline bool is_app(expr_ptr e)         { return e->kind() == expr_kind::App; }
+    // inline bool is_lambda(expr_ptr e)      { return e->kind() == expr_kind::Lambda; }
+    // inline bool is_pi(expr_ptr e)          { return e->kind() == expr_kind::Pi; }
+    // inline bool is_let(expr_ptr e)         { return e->kind() == expr_kind::Let; }
+    // inline bool is_sort(expr_ptr e)        { return e->kind() == expr_kind::Sort; }
+    // inline bool is_binding(expr_ptr e)     { return is_lambda(e) || is_pi(e); }
+    // inline bool is_mlocal(expr_ptr e)      { return is_metavar(e) || is_local(e); }
+    //
+    // inline expr_var *         to_var(expr_ptr e)        { lean_assert(is_var(e));         return static_cast<expr_var*>(e); }
+    // inline expr_const *       to_constant(expr_ptr e)   { lean_assert(is_constant(e));    return static_cast<expr_const*>(e); }
+    // inline expr_app *         to_app(expr_ptr e)        { lean_assert(is_app(e));         return static_cast<expr_app*>(e); }
+    // inline expr_binding *     to_binding(expr_ptr e)    { lean_assert(is_binding(e));     return static_cast<expr_binding*>(e); }
+    // inline expr_sort *        to_sort(expr_ptr e)       { lean_assert(is_sort(e));        return static_cast<expr_sort*>(e); }
+    // inline expr_mlocal *      to_mlocal(expr_ptr e)     { lean_assert(is_mlocal(e));      return static_cast<expr_mlocal*>(e); }
+    // inline expr_local *       to_local(expr_ptr e)      { lean_assert(is_local(e));       return static_cast<expr_local*>(e); }
+    // inline expr_mlocal *      to_metavar(expr_ptr e)    { lean_assert(is_metavar(e));     return static_cast<expr_mlocal*>(e); }
+    inline simple_expr_let const * to_simple_let(simple_expr const * e) {
+        lean_assert(simple_is_let(*e));
+        return static_cast<simple_expr_let const *>(e);
+    }
+    // inline expr_macro *       to_macro(expr_ptr e)      { lean_assert(is_macro(e));       return static_cast<expr_macro*>(e); }
+
+    name var_name(simple_expr const & se);
 }
