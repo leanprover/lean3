15e1c63dc179dfb4a99a461a126ff0194401b112
Jared Roesch <roeschinc@gmail.com>
2016-04-29 20:24:23 -0700

    feat(backends): change debugging output
    to use tracing

--- tests/lean/compile/hello_world.lean
+++ tests/lean/compile/hello_world.lean
@@ -2,20 +2,33 @@
  | mk : RealWorld
 
 structure IO (a : Type) : Type :=
-   (runIO : RealWorld → a × RealWorld)
-
+   (run : RealWorld → RealWorld × a)
 
 constant raw_string : Type
 
-constant string_to_raw_string : RealWorld -> string -> prod RealWorld raw_string
-constant raw_print : RealWorld -> raw_string -> prod RealWorld unit
+constant extern_string_to_raw_string : RealWorld -> string -> prod RealWorld raw_string
+constant extern_raw_print : RealWorld -> raw_string -> prod RealWorld unit
 
-attribute string_to_raw_string [extern]
-attribute raw_print [extern]
+definition string_to_raw_string (s : string) : IO raw_string :=
+  IO.mk (fun rw, extern_string_to_raw_string rw s)
 
-definition print_string (s : string) : prod RealWorld unit :=
-    prod.rec_on (string_to_raw_string RealWorld.mk s)
-        (fun rw rs, raw_print rw rs)
+definition raw_print (s : raw_string) : IO unit :=
+  IO.mk (fun rw, extern_raw_print rw s)
 
-definition main : prod RealWorld unit :=
+-- definition put_string (s : string) : IO unit :=
+
+definition bind {A B} (ma : IO A) (f : A -> IO B): IO B :=
+   IO.mk (fun rw, prod.rec_on ((IO.run ma) rw) (fun rw' a, IO.run (f a) rw'))
+
+
+notation ` do ` binder ` <- ` y `;` r:(scoped:1 z, z) := bind y r
+-- attribute string_to_raw_string [extern]
+-- attribute raw_print [extern]
+
+definition print_string (s : string) : IO unit :=
+   do rs <- string_to_raw_string s ;
+   raw_print rs
+
+
+noncomputable definition main : IO unit :=
     print_string "Hello Lean!"
