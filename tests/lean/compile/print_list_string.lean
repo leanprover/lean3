3feae78c3a72a62fe317bdb29ab76f90e1bf4510
Jared Roesch <roeschinc@gmail.com>
2016-05-11 00:16:35 -0700

    feat(backends): add pair test case

--- tests/lean/compile/print_list_string.lean
+++ tests/lean/compile/print_list_string.lean
@@ -4,4 +4,4 @@ definition is_empty (s : string) : string :=
     string.cases_on s "empty" (fun x y, "non-empty")
 
 definition main : IO unit :=
-    print_string (trace "app" (string_append "" ""))
+    print_string (trace "app" (string_append "" "Hello"))
