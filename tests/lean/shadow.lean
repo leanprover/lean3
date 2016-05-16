7cd87b275f5ab9b5d3cebd12755701e5b93dcf6d
Sebastian Ullrich <sebasti@nullri.ch>
2016-05-16 11:40:36 -0400

    Revert "fix(library/definitional/equations): add more equation validation to avoid obscure error message"
    This reverts commit a3bc1b0cd53e6d52212b3db1d2b6e76b4d85a9fd.

--- tests/lean/shadow.lean
+++
@@ -1,8 +0,0 @@
-open nat
-
-variable a : nat
-
--- The variable 'a' in the following definition is not the variable 'a' above
-definition tadd : nat → nat → nat
-| tadd zero     b := b
-| tadd (succ a) b := succ (tadd a b)
