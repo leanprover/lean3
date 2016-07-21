b95a03248f176d05b0a035aa67c72e341ef10435
Jared Roesch <roeschinc@gmail.com>
2016-07-20 19:30:13 -0700

    feat(library/backend): prototype compiler passes written in Lean

--- library/init/backend.lean
+++ library/init/backend.lean
@@ -1,5 +1,4 @@
 prelude
-
 import init.meta.expr
 import init.meta.unfold_tactic
 import init.monad
@@ -179,6 +178,60 @@ definition pair_list_doc_measure (p : nat × list (nat × doc)) : nat :=
 
 -- definition best (w k : nat) (d : doc) : simple_doc :=
 --    be w k [(0, x)]
+
+namespace backend
+
+inductive doc :=
+  | nil
+  | append : doc → doc → doc
+  | nest : nat → doc → doc
+  | text : string → doc
+  | line : doc
+  | group : doc → doc → doc
+
+inductive simple_doc :=
+  | nil
+  | text : string → simple_doc → simple_doc
+  | line : nat → simple_doc → simple_doc
+
+definition empty : doc :=
+  doc.nil
+
+definition concat (a b : doc) : doc :=
+  doc.append a b
+
+definition nest (n : nat) (d : doc) :=
+  doc.nest n d
+
+definition text (s : string) :=
+  doc.text s
+
+definition line := doc.line
+
+definition flatten : doc → doc
+| flatten doc.nil := doc.nil
+| flatten (doc.append x y) :=
+  doc.append (flatten x) (flatten y)
+| flatten (doc.nest n x) :=
+  doc.nest n (flatten x)
+| flatten (doc.text s) := doc.text s
+| flatten doc.line := doc.text " "
+| flatten (doc.group x y) :=
+  flatten x
+
+definition repeat {A} : nat -> A -> list A
+| repeat 0 x := []
+| repeat (nat.succ n) x := repeat n x
+
+definition layout : simple_doc → string
+| layout simple_doc.nil := ""
+| layout (simple_doc.text s d) := s ++ layout d
+| layout (simple_doc.line i x) := '\n' :: (repeat i ' ' ++ layout x)
+
+definition group (d : doc) :=
+  doc.group (flatten d) d
+
+print inductive expr
 
 meta_definition compiler (e : expr) : string :=
    to_string e
