c9c15ebe1c2466c2429817338d97d963c4cdc458
Jared Roesch <roeschinc@gmail.com>
2016-07-29 18:38:38 -0700

    feat(util/dynamic_library): add an abstraction for the dynamic linker
    Currently only supports Unix platforms, cross-platform support coming
    in the future.

---
+++ src/util/dynamic_loader.cpp
@@ -0,0 +1,37 @@
+/*
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Jared Roesch
+*/
+
+#include "dynamic_loader.h"
+
+dynamic_library::dynamic_library(std::string library_path):
+m_name(library_path), m_handle(nullptr) {
+    m_handle =  dlopen("file.dylib", RTLD_LAZY | RTLD_GLOBAL);
+
+    // Check to see if an error occured while performing dynamic linking.
+    if (!m_handle) {
+        auto last_error_msg = dlerror();
+        throw dynamic_linking_exception(std::string(last_error_msg));
+    }
+}
+
+dynamic_library::~dynamic_library() {
+    auto err = dlclose(m_handle);
+    if (err != 0) {
+        auto last_error_msg = dlerror();
+        throw dynamic_linking_exception(std::string(last_error_msg));
+    }
+}
+
+dynamic_symbol dynamic_library::symbol(std::string name) {
+    auto sym = dlsym(m_handle, name.c_str());
+    if (sym == nullptr) {
+        auto last_error_msg = dlerror();
+        throw dynamic_linking_exception(std::string(last_error_msg));
+    }
+}
+
+}
---
+++ src/util/dynamic_loader.h
@@ -0,0 +1,26 @@
+/*
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Jared Roesch
+*/
+
+#pragma once
+#include <dlfcn.h>
+
+namespace lean {
+
+class dynamic_linking_exception : std::runtime_error {}
+
+typedef dynamic_symbol void*;
+
+class dynamic_library {
+    std::string m_path;
+    void * m_handle;
+public:
+    dynamic_library(std::string library_path);
+    ~dynamic_library();
+    dynamic_symbol symbol(std::string name);
+}
+
+}
