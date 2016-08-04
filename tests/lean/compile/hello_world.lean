50cac33e9c3c2d21f35b8b734ce2f3e49d419982
Jared Roesch <roeschinc@gmail.com>
2016-08-03 17:04:45 -0700

    feat(tests/lean/native_run): implement testing for native compilation

--- tests/lean/compile/hello_world.lean
+++
@@ -1,4 +0,0 @@
-import system.io
-
-definition main : IO unit :=
-    bind (print_string "Hello Lean!") (fun x, print_string "Hello Again!")
--- tests/lean/compile/pair_string.lean
+++
@@ -1,8 +0,0 @@
-import system.io
-
-definition pair_string : prod string string :=
-    ("pair1", "pair2")
-
-definition main : IO unit :=
- (print_string (prod.pr1 pair_string))
-         -- (fun u, print_string (prod.pr2 pair_string))
