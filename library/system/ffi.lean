6d1e8474c5985aabf0cef85377b90f7b49fc8d96
Jared Roesch <roeschinc@gmail.com>
2016-08-16 18:41:41 -0700

    WIP

--- library/system/ffi.lean
+++ library/system/ffi.lean
@@ -3,6 +3,8 @@ import init.coe
 
 namespace ffi
 
+-- Be careful when changing this, there is a corresponding enum in `vm_ptr.cpp` which
+-- corresponds to the contructor numbering here.
 inductive base_type :=
 | int
 | char
@@ -10,14 +12,19 @@ inductive base_type :=
 inductive type :=
 | base : base_type → type
 | array : nat -> type → type
-| sigma : (nat → type) → type
+-- | sigma : (nat → type) → type
 | void : type
+
 
 definition base_type_coe [instance] : has_coe base_type type :=
   has_coe.mk (fun bt, type.base bt)
 
 definition cstring : type :=
-  type.sigma (fun n, type.array n (type.base (base_type.char)))
+  type.array 256 (type.base base_type.char)
+
+attribute [reducible] cstring
+
+  -- type.sigma (fun n, type.array n (type.base (base_type.char)))
 
 constant ptr : type -> Type.{1}
 
@@ -26,14 +33,22 @@ definition destructor (T : type) :=
 
 constant base_size_of (T : base_type) : nat
 
-definition sizeof (T : type) : nat := 4
+open type
+
+definition sizeof : type -> nat
+| sizeof (base bt) := base_size_of bt
+| sizeof (array n t) := n * sizeof t
+| sizeof void := 0
 
 constant allocate (T : type) (n : nat) : sizeof T = n -> (destructor T) -> IO (ptr T)
+
+constant write_array {n : nat} {T : type} (arr : ptr (array n T)) (i : nat) (value : ptr T) : IO unit
+constant read_array {n : nat} {T : type} (arr : ptr (array n T)) (i : nat) : IO (ptr T)
 
 definition new (T : type) : IO (ptr T) :=
   allocate T (sizeof T) rfl (fun x, return unit.star)
 
-open base_type
+-- open base_type
 open type
 
 definition value_of : type → Type.{1}
@@ -50,8 +65,10 @@ definition extern (ret : type) (s : string) : list type -> Type.{1}
 attribute [reducible] extern
 
 -- We really need good support for Z's would be nice to define a set of system types.
-constant write_nat_as_int : nat -> ptr int -> IO unit
-constant read_int_as_nat : ptr int -> IO nat
+constant write_nat_as_int : nat -> ptr base_type.int -> IO unit
+constant read_int_as_nat : ptr base_type.int -> IO nat
+constant write_char_as_char : char -> ptr base_type.char -> IO unit
+constant read_char_as_char : ptr base_type.char  -> IO char
 
 -- constant extern_open : extern_fn "open" [string, int] int
 -- attribute [extern] extern_open
