a6c4411f4d54ec922ab368430124173889728c24
Jared Roesch <roeschinc@gmail.com>
2016-09-12 13:49:01 -0700

    add name helper, move code to result.lean, etc

--- library/init/meta/name.lean
+++ library/init/meta/name.lean
@@ -36,12 +36,15 @@ def name.get_prefix : name → name
 | (mk_string s p)  := p
 | (mk_numeral s p) := p
 
-def name.to_string : name → string
+definition name.to_string_with_sep (sep : string) : name → string
 | anonymous                := "[anonymous]"
 | (mk_string s anonymous)  := s
 | (mk_numeral v anonymous) := to_string v
-| (mk_string s n)          := name.to_string n ++ "." ++ s
-| (mk_numeral v n)         := name.to_string n ++ "." ++ to_string v
+| (mk_string s n)          := name.to_string_with_sep n ++ "." ++ s
+| (mk_numeral v n)         := name.to_string_with_sep n ++ "." ++ to_string v
+
+definition name.to_string : name → string :=
+  name.to_string_with_sep "."
 
 instance : has_to_string name :=
 ⟨name.to_string⟩
--- library/native/default.lean
+++ library/native/default.lean
@@ -1,44 +1,14 @@
 import init.meta.format
 import init.meta.expr
 import system.IO
+import system.result
 
 namespace native
-
--- set_option new_elaborator true
 
 inductive error
 | string : string -> error
 
-inductive result : Type -> Type
-| err : Π {T}, error -> result T
-| ok : Π {T}, T -> result T
-
--- TODO: elaborator is still underway, eventually clean these up, hacking around elab bugs
-definition unwrap_or : Π {T : Type}, result T -> T -> T
-| T (result.err _) default := default
-| T (result.ok t) _ := t
-
-definition result.map : Π {T U : Type}, (T → U) → result T → result U
-| T U f (result.err e) := result.err e
-| T U f (result.ok t) := result.ok (f t)
-
-definition result.and_then : Π {T U : Type}, result T → (T -> result U) → result U
-| T U (result.err e) _ := result.err e
-| T U (result.ok t) f := f t
-
-attribute [instance]
-definition result_functor : functor result :=
-{| functor, map := @result.map |}
-
-definition result.seq : Π {T U : Type}, result (T → U) -> result T → result U
-| T U f t := result.and_then f (fun f', result.and_then t (fun t', result.ok (f' t')))
-
-attribute [instance]
-definition result_applicative : applicative result :=
-{| applicative,
-   pure := @result.ok,
-   map := @result.map,
-   seq := @result.seq |}
+definition result := system.result error
 
 inductive tag_ty
 | mk
@@ -61,7 +31,7 @@ inductive ir_expr
 inductive ir_stmt : Type
 | ite : ir_expr -> ir_stmt -> ir_stmt -> ir_stmt
 -- | switch : expr -> list (expr, stmt) -> ir_stmt -> ir_stmt
-| letb : name -> ir_stmt -> ir_stmt -> ir_stmt
+| letb : name -> ir_expr -> ir_stmt -> ir_stmt
 | e : ir_expr -> ir_stmt
 -- use a list here
 | seq : ir_stmt -> ir_stmt -> ir_stmt
@@ -72,37 +42,70 @@ inductive ir_decl
 | mk : name -> list ir_ty -> ir_ty -> ir_stmt -> ir_decl
 
 definition mk_error {T} : string -> result T :=
-  fun s, result.err $ error.string s
+  fun s, system.result.err $ error.string s
 
 definition mk_nat_literal (n : nat) : ir_stmt :=
   ir_stmt.e (ir_expr.lit $ ir_literal.nat n)
 
-definition mk_call (head : name) (e : list expr) : result ir_expr :=
-  pure $ ir_expr.call head []
+definition assert_name : ir_expr → result name
+| assert_name (ir_expr.name n) := system.result.ok n
+| assert_name e := mk_error "expected name, found: "
+
+check tactic.mk_fresh_name
+
+meta_definition anf_expr (e : expr) (bs : list (name × ir_expr)): ir_expr :=
+  match e with
+  | expr.app _ _ :=
+    let head := expr.app_fn e,
+        args := expr.get_app_args e in
+    
+
+ 
+    
+-- code looks bad, weird monad errors again, there are some issues because I'm still using the old
+-- higher order unification based elab, which creates fucking insance solutions
+definition mk_call (head : name) (args : list ir_expr) : result ir_expr :=
+  let args'' : list (result name) := list.map assert_name args in
+  do (args' : list name) <- @monad.sequence result _ _ args'', result.ok $ ir_expr.call head args'
 
 meta_definition is_return (n : name) : bool :=
   decidable.to_bool $ `native_compiler.return = n
 
-meta_definition is_constructor (n : name) :=
-  decidable.to_bool $ (mk_simple_name "_cnstr.0") = n
+meta_definition is_constructor (n : name) : bool :=
+  decidable.to_bool $ (mk_num_name (mk_simple_name "_cnstr") 0) = n
 
 meta_definition compile_expr_to_ir_expr (e : expr) : result ir_expr :=
   match e with
-  | expr.app f x :=
-    let head := expr.app_fn (expr.app f x) in
-    let args := expr.get_app_args (expr.app f x) in
-    if expr.is_constant f = bool.tt
-      then if is_return (expr.const_name f) = bool.tt
-        then compile_expr_to_ir_expr x
-        else mk_call (expr.const_name head) []
-      else mk_error "unexpected expr in head position"
    | expr.const n _ :=
      if is_constructor n = bool.tt
      -- TODO should probably restrict the space of nats
      then (pure $ ir_expr.mk_object 0 [])
      else mk_error (name.to_string n)
-   | _ := mk_error "unsupported expr in compile_expr_to_ir_expr"
+   | expr.var i := mk_error "found var"
+   | expr.sort _ := mk_error "found sort"
+   | expr.meta _ _ := mk_error "found meta"
+   | expr.local_const _ _ _ _ := mk_error "found local const"
+   | expr.app f x :=
+      let head := expr.const_name $ expr.app_fn (expr.app f x) in
+      let args := expr.get_app_args (expr.app f x) in do
+      args' <- @monad.sequence result _ _ $ list.map compile_expr_to_ir_expr args,
+      mk_call head args'
+   | expr.lam _ _ _ _ := mk_error "found lam"
+   | expr.pi _ _ _ _ := mk_error "found pi"
+   | expr.elet n _ v body := mk_error "internal error: can not translate let binding into a ir_expr"
+   | expr.macro _ _ _ := mk_call (mk_simple_name "macro") []
+   end
+
+meta_definition one_or_error (args : list expr) : result expr :=
+  match args with
+  | ((h : expr) :: []) := result.ok h
+  | _ := mk_error "internal invariant violated, should only have one argument"
   end
+
+meta_definition compile_app_expr_to_ir_stmt (head : name) (args : list expr) : result ir_stmt :=
+  if is_return head = bool.tt
+  then do rexp <- one_or_error args, ir_stmt.return <$> compile_expr_to_ir_expr rexp
+  else mk_error "can only compile return in head position"
 
 meta_definition compile_expr_to_ir_stmt (e : expr) : result ir_stmt :=
   match e with
@@ -111,10 +114,15 @@ meta_definition compile_expr_to_ir_stmt (e : expr) : result ir_stmt :=
   | expr.const n _ := mk_error "found const"
   | expr.meta _ _ := mk_error "found meta"
   | expr.local_const _ _ _ _ := mk_error "found local const"
-  | expr.app f x := result.map ir_stmt.e $ compile_expr_to_ir_expr (expr.app f x)
+  | expr.app f x :=
+    let head := expr.app_fn (expr.app f x) in
+    let args := expr.get_app_args (expr.app f x) in
+    if expr.is_constant f = bool.tt
+    then compile_app_expr_to_ir_stmt (expr.const_name f) args
+    else mk_error "unexpected expr in head position"
   | expr.lam _ _ _ _ := mk_error "found lam"
   | expr.pi _ _ _ _ := mk_error "found pi"
-  | expr.elet _ _ _ _ := mk_error "found elet"
+  | expr.elet n _ v body := ir_stmt.letb n <$> compile_expr_to_ir_expr v <*> compile_expr_to_ir_stmt v
   | expr.macro _ _ _ := mk_error "found macro"
   end
 
@@ -132,6 +140,9 @@ meta_definition compile_decl_to_ir (decl_name : name) (arity : nat) (body : expr
   result.and_then (compile_expr_to_ir_stmt body)
   (fun (body' : ir_stmt), pure (ir_decl.mk decl_name (repeat arity (ir_ty.ref ir_ty.object)) ir_ty.object body'))
 
+meta_definition mangle_name (n : name) : format :=
+to_fmt $ name.to_string_with_sep "_" n
+
 meta_definition format_ir_expr (stmt : ir_expr) : format :=
 match stmt with
 | (ir_expr.call f xs) := to_fmt f
@@ -146,6 +157,9 @@ meta_definition format_ir_stmt (stmt : ir_stmt) : format :=
 match stmt with
 | (ir_stmt.e e) := format_ir_expr e
 | (ir_stmt.return e) := format.of_string "return"  ++ format.space ++ format_ir_expr e ++ format.of_string ";"
+| (ir_stmt.letb n v body) :=
+  to_fmt "lean::vm_obj" ++ (mangle_name n) ++ (to_fmt " = ") ++ (format_ir_expr v) ++ to_fmt ";" ++
+  format.line ++ format_ir_stmt body
 | _ := format.of_string "NYI"
 end
 
@@ -176,7 +190,7 @@ meta_definition format_ir (decl : ir_decl) : format :=
   match decl with
   | ir_decl.mk n arg_tys ret_ty stmt :=
     let body := format_ir_stmt stmt in
-    (format_ir_ty ret_ty) ++ format.space ++ (to_fmt n) ++
+    (format_ir_ty ret_ty) ++ format.space ++ (mangle_name n) ++
     (format_argument_list arg_tys) ++ format.space ++
     (format.bracket "{" "}" $ format.line ++ (format.nest 4 body) ++ format.line)
   end
@@ -185,9 +199,21 @@ definition unwrap_or_else : Π {T E : Type}, result T → (T → E) → (error -
 | T E (result.err e) f err := err e
 | T E (result.ok t) f err := f t
 
+-- TODO: fix naming here
+meta_definition take_arguments' (e : expr) (ns : list name) : (list name × expr) :=
+  match e with
+  | (expr.lam n _ _ body) :=  take_arguments' body (n :: ns)
+  | e' := ([], e')
+  end
+
+meta_definition take_arguments (e : expr) : (list name × expr) :=
+  let res := take_arguments' e []
+  in (list.reverse (prod.pr1 res), prod.pr2 res)
+
 meta_definition compile_decl (decl_name : name) (e : expr) : format :=
   let arity := get_arity e in
-  let ir := compile_decl_to_ir decl_name arity e in
+  let args_and_body := take_arguments e in
+  let ir := compile_decl_to_ir decl_name arity (prod.pr2 args_and_body) in
   unwrap_or_else ir format_ir (fun e, error.cases_on e (fun s, format.of_string s))
 
 end native
