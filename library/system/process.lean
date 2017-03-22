bb104f0b32ce22b0edc286ddcbc9301773970b83
Jared Roesch <roeschinc@gmail.com>
2017-03-21 20:55:49 -0700

    fix(library/system/process): remove ugly array.from_list

--- library/system/process.lean
+++ library/system/process.lean
@@ -46,29 +46,6 @@ meta constant child.stdin : child → io (pipe pipe.mode.write)
 meta constant child.stdout : child → io (pipe pipe.mode.read)
 meta constant child.stderr : child → io (pipe pipe.mode.read)
 
-constant foo : forall {a} {i : nat}, ¬i < list.length ([] : list a)
-
-constant needed_arith {x y : nat} : x < y + 1 -> x - 1 < y
-
-def ith {α} : Π (l : list α) (i : nat), i < list.length l → α
-| []        i        h := absurd h foo -- (not_lt_zero i)
-| (a::ains) 0        h := a
-| (a::ains) (i + 1) h := ith ains i (needed_arith h) -- (lt_of_succ_lt_succ h)
-
-def list.to_fin_map {α} : forall (xs : list α), (fin (list.length xs) → α) :=
-  fun f, begin
-    intros,
-    cases a,
-    apply ith,
-    exact is_lt,
-  end
-
-def array.from_list {α} (xs : list α) : array α (xs^.length) :=
-  array.mk (list.to_fin_map xs)
-
-def array.from_string (xs : list char) : array char (xs^.reverse^.length) :=
-  array.mk (list.to_fin_map xs^.reverse)
-
 /-- Executes the command as a child process. -/
 meta constant builder.spawn : builder → io child
 
