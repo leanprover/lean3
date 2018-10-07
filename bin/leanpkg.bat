428cf7278cbb490dfefabd554d5d49996ab9a4a0
Scott Olson <sco50000@gmail.com>
2018-10-07 04:11:22 -0600

    fix(lean/bin/leanpkg.bat): handle spaces in paths
    Fixes #1973. I've tested it locally and this appears to be enough to solve the problem.

--- bin/leanpkg.bat
+++ bin/leanpkg.bat
@@ -2,8 +2,8 @@
 
 SET LEANDIR=%~dp0%../
 SET LIBDIR=%LEANDIR%\lib\lean
-IF NOT EXIST %LIBDIR% SET LIBDIR=%LEANDIR%
+IF NOT EXIST "%LIBDIR%" SET LIBDIR=%LEANDIR%
 SET LEAN_PATH=%LIBDIR%\library;%LIBDIR%\leanpkg
 SET PATH=%LEANDIR%\bin;%PATH%
 
-lean --run %LIBDIR%\leanpkg\leanpkg\main.lean %*
+lean --run "%LIBDIR%\leanpkg\leanpkg\main.lean" %*
